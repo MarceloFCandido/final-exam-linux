c3c91f6952b4697ed31fca3dc7a3018d423246f8
Marcelo Ferreira Cândido <mfcandido@ibm.com>
2021-04-08 19:50:19 -0300

    :sparkles: showing exams grades and initing to format output

--- grader.sh
+++ grader.sh
@@ -4,6 +4,7 @@ usage() {
   echo "Usage: $0 -c <class> -t <semester> -r <student-registry>"
 }
 
+# https://stackoverflow.com/a/16496491
 while getopts "hc:t:r:" o; do
   case "${o}" in
   "h")
@@ -38,16 +39,36 @@ response=$(curl -s -X POST -H "Accept: application/xml" http://rimsa.com.br/serv
 
 echo
 echo "Parsing..."
+
 IFS=$'\r\n ' name=$(echo "$response" | grep '<name>' | sed 's:[[:space:]]*<name>\(.*\)</name>:\1:')
 IFS=$'\r\n ' link=$(echo "$response" | grep '<link>' | sed 's:[[:space:]]*<link>\(.*\)</link>:\1:')
-IFS=$'\r\n ' grades_arr=($(echo "$response" | grep '<grade' | sed 's:[[:space:]]*<grade .*>\(.*\)</grade>:\1:'))
 
-grades_length=${#grades_arr[@]}
+IFS=$'\r\n ' exams_arr=($(echo "$response" | grep '<grade id="exam' | sed 's:[[:space:]]*<grade id="exam.*" .*>\(.*\)</grade>:\1:'))
+IFS=$'\r\n ' tests_arr=($(echo "$response" | grep '<grade id="test' | sed 's:[[:space:]]*<grade id="test.*" .*>\(.*\)</grade>:\1:'))
 
-tests_total=${grades_arr[0]}
-extras=${grades_arr[((grades_length - 2))]}
-total=${grades_arr[((grades_length - 1))]}
-tests_arr=(${grades_arr[@]:1:((grades_length - 3))})
+IFS=$'\r\n ' extras=$(echo "$response" | grep '<grade id="extras"' | sed 's:[[:space:]]*<grade .*>\(.*\)</grade>:\1:')
+IFS=$'\r\n ' total=$(echo "$response" | grep '<grade id="total"' | sed 's:[[:space:]]*<grade .*>\(.*\)</grade>:\1:')
 
+clear
 echo "Student: $name"
 echo "Folder link: $link"
+echo
+
+echo "Exams: "
+for ((i = 1; i < ${#exams_arr[@]}; i++)); do
+  LC_NUMERIC="en_US.UTF-8" printf "Exam %d: %f\n" $i ${exams_arr[i]}
+done
+echo "Total: ${exams_arr[0]}"
+echo
+
+echo "Tests: "
+for ((i = 1; i < ${#tests_arr[@]}; i++)); do
+  echo "Test $i: ${tests_arr[i]}"
+done
+echo "Total: ${tests_arr[0]}"
+echo
+
+echo "Extras: $extras"
+echo
+
+echo "Total: $total"
