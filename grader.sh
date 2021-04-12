8d101ff9cdd0892e1566bd85b04bf9217bca92d8
Gabriel Dutra <nesk.frz@gmail.com>
2021-04-11 21:27:57 -0300

    Use tr to format numbers with points

--- grader.sh
+++ grader.sh
@@ -45,11 +45,11 @@ echo "Parsing..."
 IFS=$'\r\n ' name=$(echo "$response" | grep '<name>' | sed 's:[[:space:]]*<name>\(.*\)</name>:\1:')
 IFS=$'\r\n ' link=$(echo "$response" | grep '<link>' | sed 's:[[:space:]]*<link>\(.*\)</link>:\1:')
 
-IFS=$'\r\n ' exams_arr=($(echo "$response" | grep '<grade id="exam' | sed 's:[[:space:]]*<grade id="exam.*" .*>\(.*\)</grade>:\1:'))
-IFS=$'\r\n ' tests_arr=($(echo "$response" | grep '<grade id="test' | sed 's:[[:space:]]*<grade id="test.*" .*>\(.*\)</grade>:\1:'))
+IFS=$'\r\n ' exams_arr=($(echo "$response" | grep '<grade id="exam' | sed 's:[[:space:]]*<grade id="exam.*" .*>\(.*\)</grade>:\1:' | tr ',' '.'))
+IFS=$'\r\n ' tests_arr=($(echo "$response" | grep '<grade id="test' | sed 's:[[:space:]]*<grade id="test.*" .*>\(.*\)</grade>:\1:' | tr ',' '.'))
 
-IFS=$'\r\n ' extras=$(echo "$response" | grep '<grade id="extras"' | sed 's:[[:space:]]*<grade .*>\(.*\)</grade>:\1:')
-IFS=$'\r\n ' total=$(echo "$response" | grep '<grade id="total"' | sed 's:[[:space:]]*<grade .*>\(.*\)</grade>:\1:')
+IFS=$'\r\n ' extras=$(echo "$response" | grep '<grade id="extras"' | sed 's:[[:space:]]*<grade .*>\(.*\)</grade>:\1:' | tr ',' '.')
+IFS=$'\r\n ' total=$(echo "$response" | grep '<grade id="total"' | sed 's:[[:space:]]*<grade .*>\(.*\)</grade>:\1:' | tr ',' '.')
 
 clear
 
@@ -68,19 +68,19 @@ echo
 
 echo "Exams: "
 for ((i = 1; i < ${#exams_arr[@]}; i++)); do
-  LC_NUMERIC="pt_BR.UTF-8" printf "%3s%02d: %05.2f\n" E $i ${exams_arr[i]}
+  printf "%3s%02d: %05.2f\n" E $i ${exams_arr[i]}
 done
-echo "Total: ${exams_arr[0]}"
+echo "Exams Total: ${exams_arr[0]}"
 echo
 
 echo "Tests: "
 for ((i = 1; i < ${#tests_arr[@]}; i++)); do
-  LC_NUMERIC="pt_BR.UTF-8" printf "%3s%02d: %05.2f\n" T $i ${tests_arr[i]}
+  printf "%3s%02d: %05.2f\n" T $i ${tests_arr[i]}
 done
-echo "Total: ${tests_arr[0]}"
+echo "Tests Total: ${tests_arr[0]}"
 echo
 
-LC_NUMERIC="pt_BR.UTF-8" printf "Extra: %05.2f\n" $extras
+printf "Extra: %05.2f\n" $extras
 echo
 
-LC_NUMERIC="pt_BR.UTF-8" printf "Total: %05.2f\n" $total
+printf "Total: %05.2f\n" $total
